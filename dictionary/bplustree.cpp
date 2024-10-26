1b44dc375932049db9cdcb3b3d0cf53c9376b1bb
muhaimeen90 <alamuhaimen@gmail.com>
2024-10-26 22:09:47 +0600

    ,

---
+++ dictionary/bplustree.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    vector<string> keys;
+    vector<Node> *nodes;
+    bool leaf;
+
+}
+
+int
+main()
+{
+    int keys = 4;
+    number
+}
